0d2f02a946130355a63b0d1b98553863b5f14ada
devhabeeblateef <77229868+devhabeeblateef@users.noreply.github.com>
2022-02-05 03:25:42 +0100

    Another Commit

--- src/App.js
+++ src/App.js
@@ -3,30 +3,63 @@ import './App.css';
 import ReactDOM from 'react-dom';
 import Background from './Components/Background';
 import backgroundVideo from './video/video2.mp4';
-import avatar from './Image/habeeb.jfif';
+import avatar from './Image/mock.png';
 
-function App() {
+function BG() {
   return (
     <div>
       <video autoPlay loop muted id="video" class="grayscale">
         <source src={backgroundVideo} type="video/mp4" />
       </video>
+    </div>
+)}
 
+function Avatar() {
+  return (
+    <div>
       <div class="overlay">
       <div id="outerContainer">
-    <div id="container">
+      <div id="container">
       <div class="item">
         <img src={avatar} />
       </div>
-    <div class="circle" id="One"></div>
-    <div class="circle" id="Two"></div>
-    <div class="circle" id="Three"></div>
-    <div class="circle" id="Four"></div>
-    </div>
-  </div>
+      <div class="circle" id="One"></div>
+      <div class="circle" id="Two"></div>
+      <div class="circle" id="Three"></div>
+      <div class="circle" id="Four"></div>
       </div>
-  </div>
+      </div>
+      </div>
+    </div>
   );
+}
+
+function Bio()
+{
+  return (
+    <div>
+      <div class="overlay">
+      <div class="message">
+      <h2 class="head">Hello.</h2>
+      <h1 class="des">-</h1> 
+      
+      <h3 class="subHead">Lateef Habeeb</h3>
+      <p class="theBody">Front End Web Developer</p>
+      </div>
+      </div>
+    </div>
+  )
+}
+
+function App()
+{
+  return (
+    <div>
+      <BG />
+      <Avatar />
+      <Bio />
+    </div>
+  )
 }
 
 ReactDOM.render(
